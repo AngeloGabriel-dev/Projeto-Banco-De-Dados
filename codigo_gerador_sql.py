1856721d61336429d5f564fe3b7fe7ae7fbfdbba
Luan Moreno <luanpmoreno@hotmail.com>
2024-05-22 13:15:15 -0300

    Correção do código + Queries de Criação de Tabelas

--- codigo_gerador_sql.py
+++ codigo_gerador_sql.py
@@ -66,58 +66,58 @@ def cria_tabelas_sql(final_cursos=final_cursos, final_disciplinas = final_discip
     orcamento = round(uniform(10000, 100000), 2)
     edificio = edificios[aleatorio_cursos]
 
-    print(f"insert into departamento(dept_nome, orcamento, edificio) values ('{dept_nome}', '{orcamento}', '{edificio}')")
+    print(f"\ninsert into departamento(dept_nome, orcamento, edificio) values ('{dept_nome}', '{orcamento}', '{edificio}')\n")
 
     id = ids.pop(aleatorio)
     nome = nomes_p.pop(aleatorio)
     salario = round(uniform(5000, 20000), 2)
     eh_chefe = choice([True, False])
 
-    print(f"insert into professor(id, nome, dept_nome, salario, eh_chefe) values ('{id}', '{nome}', '{dept_nome}', '{salario}', {eh_chefe})")
+    print(f"insert into professor(id, nome, dept_nome, salario, eh_chefe) values ('{id}', '{nome}', '{dept_nome}', '{salario}', {eh_chefe})\n")
 
     aleatorio = randint(0, finall)
     tcc_id = tcc_ids.pop(aleatorio)
-    print(f"insert into grupo_tcc(tcc_id, id) values ('{tcc_id}', '{id}')")
+    print(f"insert into grupo_tcc(tcc_id, id) values ('{tcc_id}', '{id}')\n")
 
     aleatorio = randint(0, finall)
     ra = ras.pop(aleatorio)
     dept_nome = departamentos[aleatorio_cursos]
     nome = nomes_a.pop(aleatorio)   
 
-    print(f"insert into aluno(ra, dept_nome, nome, tcc_id) values ('{ra}', '{dept_nome}', '{nome}', '{tcc_id}')")
+    print(f"insert into aluno(ra, dept_nome, nome, tcc_id) values ('{ra}', '{dept_nome}', '{nome}', '{tcc_id}')\n")
 
-    print(f"insert into orientado(p_id, a_ra) values ('{id}', '{ra}')")
+    print(f"insert into orientado(p_id, a_ra) values ('{id}', '{ra}')\n")
     
     id_historico_a = ids_historico_a.pop(aleatorio)
 
-    print(f"insert into historico_aluno(id_historico_a, ra) values ('{id_historico_a}', '{ra}')")
+    print(f"insert into historico_aluno(id_historico_a, ra) values ('{id_historico_a}', '{ra}')\n")
     
     id_historico_p = ids_historico_p.pop(aleatorio)
 
-    print(f"insert into historico_professor(id_historico_p, id) values ('{id_historico_p}', '{id}')")
+    print(f"insert into historico_professor(id_historico_p, id) values ('{id_historico_p}', '{id}')\n")
     
     id_curso = ids_curso[aleatorio_cursos]
     titulo = cursos[aleatorio_cursos]
 
-    print(f"insert into curso(id_curso, dept_nome, titulo) values ('{id_curso}', '{dept_nome}', '{titulo}')")
+    print(f"insert into curso(id_curso, dept_nome, titulo) values ('{id_curso}', '{dept_nome}', '{titulo}')\n")
     
     id_disciplina = ids_disciplina[aleatorio_disciplinas]
     nome = disciplinas[aleatorio_disciplinas]
 
-    print(f"insert into disciplina(id_disciplina, nome, id_curso) values ('{id_disciplina}', '{nome}', '{id_curso}')")
+    print(f"insert into disciplina(id_disciplina, nome) values ('{id_disciplina}', '{nome}')\n")
 
-    print(f"insert into matriz_curricular(id_curso, id_disciplina) values ('{id_curso}', '{id_disciplina}')")
+    print(f"insert into matriz_curricular(id_curso, id_disciplina) values ('{id_curso}', '{id_disciplina}')\n")
 
     id_historico = id_historico_a
     semestre = choice([1, 2])
     ano = randint(1990, 2024)
     media = randint(0, 10)
 
-    print(f"insert into estudou(id_historico, id_disciplina, semestre, ano, media) values ('{id_historico}', '{id_disciplina}', '{semestre}', '{ano}', '{media}')")
+    print(f"insert into estudou(id_historico, id_disciplina, semestre, ano, media) values ('{id_historico}', '{id_disciplina}', '{semestre}', '{ano}', '{media}')\n")
 
     id_historico = id_historico_p
 
-    print(f"insert into lecionou(id_historico, id_disciplina, semestre, ano) values ('{id_historico}', '{id_disciplina}', '{semestre}', '{ano}')")
+    print(f"insert into lecionou(id_historico, id_disciplina, semestre, ano) values ('{id_historico}', '{id_disciplina}', '{semestre}', '{ano}')\n")
     finall -= 1
 
 for x in range(1):
